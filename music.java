24e4315e931735b086da2c3a2b460d33baaa38c1
avata <avatarsjc@yahoo.com>
2020-04-19 10:13:30 -0700

    modified the Mobil.java file inside the First Branch

--- music.java
+++ music.java
@@ -1,3 +1,48 @@
-class music {
 
-}
+avata@DESKTOP-P22UTKL MINGW64 ~/Desktop/Android-Git (master)
+$ git commit -a -m "commiting multiple files"
+[master 20ce1ca] commiting multiple files
+ 3 files changed, 0 insertions(+), 0 deletions(-)
+ create mode 100644 Lion.java
+ create mode 100644 paint.java
+ create mode 100644 song.java
+
+avata@DESKTOP-P22UTKL MINGW64 ~/Desktop/Android-Git (master)
+$ git status
+On branch master
+nothing to commit, working tree clean
+
+avata@DESKTOP-P22UTKL MINGW64 ~/Desktop/Android-Git (master)
+$ git log
+commit 20ce1ca5de933ff589c1e05926059cb465e3a873 (HEAD -> master)
+Author: avata <avatarsjc@yahoo.com>
+Date:   Sat Apr 18 13:07:31 2020 -0700
+
+    commiting multiple files
+
+commit 69a8c328d37e4908aa10c5262422227351f65dc4 (origin/master)
+Author: avata <avatarsjc@yahoo.com>
+Date:   Sat Apr 18 12:17:28 2020 -0700
+
+    added class music
+
+commit ee25f2c40988565ccc4971193a368095f9d2a12e
+Author: avata <avatarsjc@yahoo.com>
+Date:   Sat Apr 18 11:39:24 2020 -0700
+
+    This is my first commit
+
+commit c558dc366eb27ab992b9a9cf706befebfab37f81
+Author: avatarsjc <avatarsjc@gmail.com>
+Date:   Sat Apr 18 09:16:45 2020 -0700
+
+    Update README.md
+
+commit 5bb449a521814ad95fc59ad295c107f3eab796bb
+Author: avatarsjc <avatarsjc@gmail.com>
+Date:   Sat Apr 18 09:12:53 2020 -0700
+
+    Initial commit
+
+avata@DESKTOP-P22UTKL MINGW64 ~/Desktop/Android-Git (master)
+$
